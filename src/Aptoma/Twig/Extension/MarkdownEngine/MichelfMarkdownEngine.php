1c250bd3d70acb469eae9a375e41dbe4850444c6
Pete Favelle <ahnlak@gmail.com>
2015-11-05 17:08:01 +0000

    Extended the MichelfMarkdownEngine to allow a few useful construction parameters

--- src/Aptoma/Twig/Extension/MarkdownEngine/MichelfMarkdownEngine.php
+++ src/Aptoma/Twig/Extension/MarkdownEngine/MichelfMarkdownEngine.php
@@ -15,11 +15,28 @@ use Michelf\MarkdownExtra;
 class MichelfMarkdownEngine implements MarkdownEngineInterface
 {
     /**
+     * Constructor
+     * 
+     * @param bool $no_markup   prevents HTML tags in the input 
+     *                          being passed to the output
+     * @param bool $no_entities prevents HTML entities being
+     *                          passed verbatim to the output
+     * $param string $url_filter_func filter function for URLs
+     */
+    public function __construct( $no_markup = true, $no_entities = true, $url_filter_func = null )
+    {
+        $this->parser = new MarkdownExtra;
+        $this->parser->no_markup = $no_markup;
+        $this->parser->no_entities = $no_entities;
+        $this->parser->url_filter_func = $url_filter_func;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function transform($content)
     {
-        return MarkdownExtra::defaultTransform($content);
+        return $this->parser->transform($content);
     }
 
     /**
@@ -29,4 +46,6 @@ class MichelfMarkdownEngine implements MarkdownEngineInterface
     {
         return 'Michelf\Markdown';
     }
+
+    private $parser;
 }
